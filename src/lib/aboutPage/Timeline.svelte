da02279f3f073724913a768a45d559d2eaaa3672
Fawzy Mohamed <fmohamed@alkholi.com>
2023-09-26 13:02:55 +0300

    finish the timeline section of the about page

---
+++ src/lib/aboutPage/Timeline.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import LL from '$i18n/i18n-svelte';
+	import { currentAppLang } from '$lib/stores/store';
+	import { pageDirection } from '$lib/stores/store';
+</script>
+
+<section class="container px-3 mx-auto py-5 2xl:py-8">
+	<!-- title -->
+	<div class="text-center">
+		<h2
+			class="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl 2xl:text-[55px] font-semibold text-primary-500 mb-8 xl:mb-12"
+		>
+			TIMELINE
+		</h2>
+	</div>
+
+	<div>
+		<p
+			class="text-sm sm:text-base md:text-lg lg:px-8 2xl:px-24 leading-8 text-justify md:text-center"
+		>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, praesentium, inventore
+			vel, aspernatur ab velit laborum rem dolorum rerum commodi facere sequi iusto tempora?
+			Necessitatibus, architecto. Quos hic atque totam similique perspiciatis impedit eum deleniti
+			quas nemo, dolore assumenda ex soluta ipsam quam eligendi rem, dolor praesentium illum esse
+			voluptatum velit voluptatibus illo, culpa ipsa. Nihil dicta assumenda reiciendis incidunt.
+		</p>
+	</div>
+</section>
--- src/routes/[lang=lang]/about/+page.svelte
+++ src/routes/[lang=lang]/about/+page.svelte
@@ -2,6 +2,7 @@
 	import LL from '$i18n/i18n-svelte';
 	import AboutUs from '$lib/aboutPage/AboutUs.svelte';
 	import HeroSection from '$lib/aboutPage/HeroSection.svelte';
+	import Timeline from '$lib/aboutPage/Timeline.svelte';
 	import SectionSize from '$lib/generalComponents/SectionSize.svelte';
 	import { currentAppLang } from '$lib/stores/store';
 	import { pageDirection } from '$lib/stores/store';
@@ -11,3 +12,7 @@
 <HeroSection />
 
 <AboutUs sectionData={data.aboutSectionData} />
+
+<Timeline />
+
+<div class="h-96 w-full" />
