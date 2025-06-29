8b549e374f3bc610efc9123d5dc5ed9ac3fdfaec
Fawzy Mohamed <fmohamed@alkholi.com>
2025-06-29 16:48:06 +0300

    updating the ISO logos

--- src/lib/homePage/ISOLogos.svelte
+++ src/lib/homePage/ISOLogos.svelte
@@ -9,15 +9,15 @@
 		<div
 			class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-8 md:gap-12 xl:gap-8"
 		>
-			<!-- <div>
+			<div>
 				<img
-					src="/images/home/isoSection/BV_Certification_ISO_300x150.webp"
-					srcset="/images/home/isoSection/BV_Certification_ISO_200x100.webp 768w, 
-          /images/home/isoSection/BV_Certification_ISO_300x150.webp 2000w"
+					src="/images/home/isoSection/BV_Cert_ISO_9001_UKAS_300x150.webp"
+					srcset="/images/home/isoSection/BV_Cert_ISO_9001_UKAS_200x100.webp 768w, 
+          /images/home/isoSection/BV_Cert_ISO_9001_UKAS_300x150.webp 2000w"
 					alt="iso logo"
 					class="w-full aspect-[1/0.5] py-2"
 				/>
-			</div> -->
+			</div>
 
 			<div>
 				<img
@@ -29,35 +29,45 @@
 				/>
 			</div>
 
-			<!-- <div>
+			<div>
 				<img
-					src="/images/home/isoSection/UKAS_300x150.webp"
-					srcset="/images/home/isoSection/UKAS_200x100.webp 768w, 
-          /images/home/isoSection/UKAS_300x150.webp 2000w"
+					src="/images/home/isoSection/BV_Cert_ISO 45001_2018_UKAS_300x150.webp"
+					srcset="/images/home/isoSection/BV_Cert_ISO 45001_2018_UKAS_200x100.webp 768w, 
+          /images/home/isoSection/BV_Cert_ISO 45001_2018_UKAS_300x150.webp 2000w"
 					alt="iso logo"
 					class="w-full aspect-[1/0.5]"
 				/>
-			</div> -->
+			</div>
 
-			<!-- <div>
+			<div>
 				<img
-					src="/images/home/isoSection/BV_Certification_ISO14001_300x150.webp"
-					srcset="/images/home/isoSection/BV_Certification_ISO14001_200x100.webp 768w, 
-          /images/home/isoSection/BV_Certification_ISO14001_300x150.webp 2000w"
+					src="/images/home/isoSection/BV_Cert_ISO_14001-2015-UKAS_300x150.webp"
+					srcset="/images/home/isoSection/BV_Cert_ISO_14001-2015-UKAS_200x100.webp 768w, 
+          /images/home/isoSection/BV_Cert_ISO_14001-2015-UKAS_300x150.webp 2000w"
 					alt="iso logo"
 					class="w-full aspect-[1/0.5] py-2"
 				/>
-			</div> -->
+			</div>
 
-			<!-- <div>
+			<div>
 				<img
-					src="/images/home/isoSection/BV_Cert_ISO_300x150.webp"
-					srcset="/images/home/isoSection/BV_Cert_ISO_200x100.webp 768w, 
-          /images/home/isoSection/BV_Cert_ISO_300x150.webp 2000w"
+					src="/images/home/isoSection/BVC_Logo_300x150.webp"
+					srcset="/images/home/isoSection/BVC_Logo_200x100.webp 768w, 
+          /images/home/isoSection/BVC_Logo_300x150.webp 2000w"
 					alt="iso logo"
 					class="w-full aspect-[1/0.5] py-2"
 				/>
-			</div> -->
+			</div>
+
+			<div>
+				<img
+					src="/images/home/isoSection/BV_Cert_ISO_9001_ISO_14001_ISO_45001_300x150.webp"
+					srcset="/images/home/isoSection/BV_Cert_ISO_9001_ISO_14001_ISO_45001_200x100.webp 768w, 
+          /images/home/isoSection/BV_Cert_ISO_9001_ISO_14001_ISO_45001_300x150.webp 2000w"
+					alt="iso logo"
+					class="w-full aspect-[1/0.5] py-2"
+				/>
+			</div>
 		</div>
 	</div>
 </section>
